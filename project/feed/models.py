2a37f2ffab89b849c9f599aa856509626463208e
rajvijay <raju.rajvijay619@gmail.com>
2020-04-23 00:23:34 +0530

    add created field to post model

--- project/feed/models.py
+++ project/feed/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 import os 
 from taggit.managers import TaggableManager
-
+from datetime import datetime
 # Create your models here.
 
 
@@ -15,6 +15,7 @@ class post(models.Model):
     author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='post_photos/%d/%m/%Y',blank=True,default=DEFAULT_POST_PHOTO)
     tags = TaggableManager()
+    created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
